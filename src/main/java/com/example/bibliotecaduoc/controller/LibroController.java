b8dd5f98274c6ecd39aaa4a7ce6dbfe3fcc79a38
Barrolas <barrab.nicolas@gmail.com>
2025-05-02 20:46:52 -0400

    [ CONFIG ]: COMMIT 002 Libreria cargada

--- src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -1,5 +1,50 @@
 package com.example.bibliotecaduoc.controller;
+import com.example.bibliotecaduoc.model.Libro;
+import com.example.bibliotecaduoc.service.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+
+@RestController
+@RequestMapping ("/api/v1/libros")
 public class LibroController {
+    @Autowired
+    private LibroService libroService;
 
+    @GetMapping("path")
+    public List<Libro> listarLibros(){
+        return libroService.getLibros();
+    }
+    
+    
+    @PostMapping("path")
+    public Libro agregarLibro(@RequestBody Libro libro){
+        return libroService.saveLibro(libro);
+    }
+    
+    @GetMapping("path")
+    public Libro buscarLibro(@PathVariable int id){
+        return libroService.getLibroId(id);
+    }
+    
+    @PutMapping
+    public Libro actualizarLibro(@PathVariable int id, @RequestBody Libro libro){
+        return libroService.updateLibro(libro);
+    }
+    
+    @DeleteMapping
+    public String eliminarLibro(@PathVariable int id){
+        return libroService.deleteLibro(id);
+    }
+    
 }
--- src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -1,5 +1,83 @@
 package com.example.bibliotecaduoc.repository;
+import com.example.bibliotecaduoc.model.Libro;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class LibroRepository {
+    
+    private List<Libro> Listalibros = new ArrayList<>();
 
+
+    public List<Libro> obtenerLibros(){
+        return Listalibros;
+    }
+    
+    public Libro buscarPorId(int id){
+        for (Libro libro : Listalibros){
+            if (libro.getId() == id){
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    public Libro buscarPorIsbn(String isbn){
+        for (Libro libro : Listalibros){
+            if (libro.getIsbn().equals(isbn)) {
+                return libro;
+            }
+
+        }
+        return null;
+    }
+    public Libro guardar (Libro lib){
+        Listalibros.add(lib);
+        return lib;
+    }
+
+    public Libro actualizar(Libro lib){
+        int id = 0;
+        int idPosicion = 0;
+        for (int i = 0; i < Listalibros.size(); i++){
+            if (Listalibros.get(i).getId() == lib.getId()) {
+                id = lib.getId();
+                idPosicion = i;
+                
+            }
+        }
+
+        Libro libro1 = new Libro();
+        libro1.setId(id);
+        libro1.setTitulo(lib.getTitulo());
+        libro1.setAutor(lib.getAutor());
+        libro1.setFechaPublicacion(lib.getFechaPublicacion());;
+        libro1.setEditorial(lib.getEditorial());
+        libro1.setIsbn(lib.getIsbn());
+
+        Listalibros.set(idPosicion, libro1);
+        return libro1;
+      }
+      
+      public void eliminar(int id){
+        Libro libro = buscarPorId(id);
+        if (libro != null) {
+            Listalibros.remove(libro);
+        }
+
+        int idPosicion = 0;
+        for(int i = 0; i < Listalibros.size(); i++){
+            if (Listalibros.get(i).getId() == id) {
+                idPosicion = i;
+                break;
+            }
+        }
+        if (idPosicion > 0) {
+            Listalibros.remove(idPosicion);
+        }
+
+        Listalibros.removeIf(x -> x.getId() == id);
+      }
 }
